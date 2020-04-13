fc7f686bda82f65a142c21b4802714fc66f746fa
ScheinerS <santiagoscheiner@hotmail.com>
2020-04-13 18:31:27 -0300

    Ya funciona el bloque que cuenta el tamaño de los clusters y guarda la tabla en TXT.

--- 1-1.c
+++ 1-1.c
@@ -6,20 +6,21 @@
 #include <string.h>
 
 #define SEED 26//085
-#define PROB 0.5
-#define L 32
+#define PROB 0.55
+#define L 6
 
 double myrandom(double prob);
 int min(int s1,int s2);
 int max(int s1,int s2);
 int etiqueta_verdadera(int *clase, int s);
 int imprimir(int *red,int N);
+int guardar_datos(int *X, int *Y);
 
 int main()
 {
 	int    i,s,s1,s2,frag,j,N;
 	double prob,r;
-	int *red,*clase;
+	int *red, *clase, *tamano, *etiquetas;
 	s =1;
 	s1=0;	// Va a ser el vecino de arriba.
 	s2=0;	// Va a ser el vecino de la izquierda.
@@ -42,9 +43,10 @@ int main()
 			else       
 				*(red+i)=1;
 		}
-	  imprimir(red,N);
+	printf("Red:\n");
+	imprimir(red,N);
 
-	  printf("\n");
+	printf("\n");
 
 	for(s=0;s<N*N;s++)
 	{
@@ -83,13 +85,13 @@ int main()
 	}
 	
 	// Analizamos el resto de la matriz:
-	for(i=1;i<N;i++)
-	{ for(j=1;j<N;j++)
-		{ s1 = *(red+(i*N+j-1)); //El casillero de la izquierda.
-		  s2 = *(red+(i*N+j-N)); //El casillero de arriba.
+	for(i=1;i<N;i++){
+		for(j=1;j<N;j++){
+			s1 = *(red+(i*N+j-1)); //El casillero de la izquierda.
+			s2 = *(red+(i*N+j-N)); //El casillero de arriba.
 
-		  if( *(red+(i*N+j)) && (s1 || s2))
-			  { if (s1 && s2 && (etiqueta_verdadera(clase, s2)<etiqueta_verdadera(clase, s1))) // Los dos son 1 y s1 mayor.
+			if( *(red+(i*N+j)) && (s1 || s2)){
+				if (s1 && s2 && (etiqueta_verdadera(clase, s2)<etiqueta_verdadera(clase, s1))) // Los dos son 1 y s1 mayor.
 					{ *(red+(i*N+j)) = etiqueta_verdadera(clase, s2);
 					  *(clase+s1) = -abs(*(clase+s2));
 					}
@@ -114,41 +116,83 @@ int main()
 		  }
 		}
 	}
-
+	
+	
 	for(i=0; i<N*N; i++)
 		{ *(clase+i)=etiqueta_verdadera(clase, i);
 		}
 
 
-	for(i=0;i<N*N;i++)
-		{ *(red+i) = *(clase+*(red+i));
+	for(i=0;i<N*N;i++){
+		*(red+i) = *(clase+*(red+i));
 		}
 
 	free(clase);
+	
+	printf("Red clasificada:\n");
+	
+	imprimir(red,N);
 
-	  imprimir(red,N);
+	printf("\n");
 
-	  printf("\n");
+
+	// Contamos la cantidad de clusters que existen con "i" componentes:
+	etiquetas = (int*)malloc((N*N)*sizeof(int));	// vector con las etiquetas posibles: {2, 3, ..., N*N/2}.
+	tamano = (int*)malloc((N*N)*sizeof(int));	// vector con las cantidades de clusters que hay de cada tamaño: cantidad[i] = # clusters de tamaño "i".
+	int count = 0;
+	int e;
+	etiquetas[0] = etiquetas[1] = 0;	// no hay clustes con etiquetas 0 y 1.
+	
+	printf("Etiqueta\tTamaño del cluster\n");
+
+	// Barremos la matriz una vez por cada etiqueta y contamos cuántas veces encontramos cada etiqueta.
+	// DEBE HABER UNA FORMA MÁS EFICIENTE, PERO NO SE ME OCURRE.
+
+	for (e=2;e<N*N/2+1;e++){
+		etiquetas[e] = e;
+		for(i=0;i<N;i++){
+			for(j=0;j<N;j++){
+				// Si la clase en ese lugar coincide con la etiqueta que estamos buscando, incrementamos la cuenta:
+				if(*(red+(i*N+j))==e){
+					count++;
+					}					
+				}
+			}
+		tamano[e]=count;
+		count = 0;	// Reiniciamos la cuenta para la siguiente etiqueta.
+		printf("%i\t\t%i\n",etiquetas[e],tamano[e]);
+		}
+	
+	// Guardamos los datos en el archivo:
+	guardar_datos(etiquetas,tamano);
+	
+	return 0;
+}
+
+
+
+
+int guardar_datos(int *X, int *Y){
+
+	char filename[255];
+	sprintf(filename,"Tamaños_L=32.txt");
+	// sprintf(filename,"Tamaños_L=32.txt");
+	
+	// Creamos el archivo:
+	FILE *fp=fopen(filename,"w");
+	
+	int i;
+	for(i = 0; i < L*L/2+1; i++)
+	  {
+	  fprintf(fp,"%4d\t%4d\n",*(X+i),*(Y+i));
+	  }
+	
+	// Cerramos el archivo:
+	fclose(fp);
 
 	return 0;
 }
 
-void guardar_datos(X,Y){
-	// Creamos el archivo:
-	char filename[255];
-	sprintf(filename,"Tamaños_L=32.txt");
-	FILE *fp=fopen(filename,"w");
-
-	// Entonces supongamos que ustedes ahora tienen dos punteros Y y X de N componentes cada uno y quieren graficar Y vs X. Entonces hacen lo siguiente
-
-	for(i = 0; i < N; i++)
-	  {
-	  fprintf(fp,"%lf %lf",*(X+i),*(Y+i));
-	  }
-	
-	// cierran el archivo.
-	fclose(fp); 
-}
 
 int etiqueta_verdadera(int *clase, int s)
 	{ while(*(clase+s)<0){
