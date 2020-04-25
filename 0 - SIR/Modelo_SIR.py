7d942acdca3e72a635ac2de4e0e598ff1d974d01
ScheinerS <santiagoscheiner@hotmail.com>
2020-04-25 16:36:34 -0300

    Gráficos con R_0=3, 6, 9, 18.

--- 0 - SIR/Modelo_SIR.py
+++ 0 - SIR/Modelo_SIR.py
@@ -8,15 +8,27 @@ Created on Wed Apr 22 10:54:45 2020
 import scipy.integrate as spi
 import numpy as np
 import matplotlib.pyplot as plt
-'''tamaño poblacional'''
-N=1#número de la población
-tau=0.6#prob de infectar dado el contacto
-C=1/5#frecuencia de los contactos
-d=30#duración de la infectividad
 
-beta=tau*C
-nu=1/d
-R_0=beta/nu
+plt.close('all')
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+TitleSize = 20
+AxisLabelSize = 16
+LegendSize = 14
+NumberSize = 10
+
+#%%
+
+'''tamaño poblacional'''
+N = 1     # cantidad de individuos de la población
+tau = 0.6 # prob de infectar dado el contacto
+C = 1/6   # frecuencia de los contactos
+d = 30    # duración de la infectividad
+
+beta = tau*C
+nu = 1/d
+R_0 = beta/nu
 
 '''condiciones iniciales'''
 TS=1
@@ -37,15 +49,25 @@ def diff_eqs(INP,t):
 t_start = 0.0; t_end = ND; t_inc = TS
 t_range = np.arange(t_start, t_end+t_inc, t_inc)
 RES = spi.odeint(diff_eqs,INPUT,t_range)
-print('R_0:')
-print(beta/nu)
-#Gráfica
-plt.plot(RES[:,0]*N, '-g', label='Susceptibles')
+print('R_0: %.2f'%R_0)
+
+
+#%%
+
+plt.figure()
+
+
+plt.plot(RES[:,0]*N, color='orange', label='Susceptibles')
 plt.plot(RES[:,2]*N, '-k', label='Recuperados')
 plt.plot(RES[:,1]*N, '-r', label='Infectados')
-plt.legend(loc=0)
-plt.title('Modelo SIR')
-plt.xlabel('Tiempo')
-plt.grid()
-'''guardamos la imagen'''
-#plt.savefig('sirpy')
+
+plt.legend(loc='best', fontsize=LegendSize)
+plt.title(r'$R_{0} = %.2f$'%(R_0), fontsize=TitleSize)
+
+plt.xlabel(r'Tiempo (d\'ias)', fontsize=AxisLabelSize)
+plt.ylabel(r'Proporci\'on de la poblaci\'on', fontsize=AxisLabelSize)
+
+plt.grid(axis='both', color='k', linestyle='dashed', linewidth=2, alpha=0.2)
+plt.show()
+
+plt.savefig('SIR-R_0=%.2f.png'%(R_0))
