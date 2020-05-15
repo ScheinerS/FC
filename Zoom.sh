1e1343b7d208baedf5612f65a2b8a8d0f59f3a63
Santiago <santiagoscheiner@gmail.com>
2020-05-15 09:13:37 -0300

    Borré Zoom.sh

--- Zoom.sh
+++
@@ -1,9 +0,0 @@
-#Clase de FC:
-
-# Teóricas:		Miércoles	9 a 11 hs	(A CONFIRMAR)
-# Prácticas:	Viernes		9 a 11 hs	(A CONFIRMAR)
-
-firefox		zoom.us/my/df.aula09
-sleep 5
-firefox		zoom.us/my/df.aula09
-# passwd:		exactas20
