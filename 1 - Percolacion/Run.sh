c74adeb272a89a89552aebf8f6b66d04abe97473
ScheinerS <santiagoscheiner@gmail.com>
2020-05-22 11:37:59 -0300

    Agregué gamma.c para L=128 y el bloque para apagar la máquina.

--- 1 - Percolacion/Run.sh
+++ 1 - Percolacion/Run.sh
@@ -1,12 +1,31 @@
-run () {
-echo '\n'
-echo $1
-echo '\n'
-gcc -Wall -o $1.e $1.c -lm
-./$1.e
+
+alarm(){
+for i in 1 2 3 4 5
+do
+	echo '\a'
+	sleep 1
+
+done
 }
+
+run () {
+	echo '\n'
+	echo $1
+	echo '\n'
+	gcc -Wall -o $1.e $1.c -lm
+	./$1.e
+}
+
+#################################
+# PROGRAMAS A CORRER:
 
 run Percolacion
 #run Armar_histograma
 #run clustersfinal
-#run 1a_M
+#run gamma
+
+#################################
+
+# Para apagar la máquina al finalizar:
+alarm
+shutdown
