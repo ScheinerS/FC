2b37b070283a6bc736be116f521a01c279742eb6
ScheinerS <santiagoscheiner@gmail.com>
2020-06-18 17:44:44 -0300

    Agregué una línea para graficar distintas etapas de la matriz. Hay que seguirlo.

--- 2 - Ising/Códigos/Ising/Datos/Graficar_matriz.py
+++ 2 - Ising/Códigos/Ising/Datos/Graficar_matriz.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 
 L=32
 
-red = np.loadtxt("red_L=%d.txt"%L)#, dtype='i', delimiter=' ')
+filename = "red_L=%d.txt"%L
+#filename = "red_L=%d.txt"%L
+
+red = np.loadtxt(filename)#, dtype='i', delimiter=' ')
 
 plt.imshow(red,cmap='hot_r') # gist_heat_r, hot_r # poner cualquier cosa para ver lista completa.
     #coolwarm, viridis, plasma, inferno, magma, cividis
