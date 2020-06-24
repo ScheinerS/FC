bf2552b70344888bbc2c1f8c8cf698cdac8abf5b
ScheinerS <santiagoscheiner@gmail.com>
2020-06-24 20:11:50 -0300

    Título corregido.

--- 2 - Ising/Códigos/Montecarlo/Correlacion.py
+++ 2 - Ising/Códigos/Montecarlo/Correlacion.py
@@ -70,7 +70,7 @@ for delta in [0.1, 1]:
 
 plt.xlabel(r'Pasos', fontsize=AxisLabelSize)
 plt.ylabel(r'Correlaci\'on', fontsize=AxisLabelSize)
-plt.title(r'$\delta = %g$'%delta, fontsize=TitleSize)
+#plt.title(r'$\delta = %g$'%delta, fontsize=TitleSize)
 plt.legend(loc='best', fontsize=LegendSize)
 plt.grid(axis='both', color='k', linestyle='dashed', linewidth=2, alpha=0.2)
 plt.show()
