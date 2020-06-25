0e879fe4f777c8aef8f9a8ce2561800cd6946ceb
ScheinerS <santiagoscheiner@gmail.com>
2020-06-25 20:14:56 -0300

    Gráficos de energía y magnetización.

--- 2 - Ising/Códigos/Ising/Datos/Espines_sin_interaccion.py
+++ 2 - Ising/Códigos/Ising/Datos/Espines_sin_interaccion.py
@@ -27,20 +27,23 @@ plt.rc('font', family='serif')
 #%%
 # FALTA AGREGAR CUANDO ESTÉN HECHOS:
 
+# Tamaño de la red:
+L=32
+
 # Datos de la simulación:
 # magnetizacion = np.loadtxt('   .csv', skiprows=1, delimiter=' ', unpack=True)
 # energia = np.loadtxt('   .csv', skiprows=1, delimiter=' ', unpack=True)
 
-L=32
-# Valor esperado:
-B = np.linspace(0, 5, 100)
+# Valores esperados:
+B = np.linspace(0, 5, 200)
 magnetizacion = np.tanh(B)
-
+energia = -B*np.tanh(B)*L*L
+#susceptibilidad = -B*np.tanh(B)*L*L
 #%%
 # Gráfico de magnetización:
 
 plt.figure()
-#plt.plot(, , 'ok', label=r'Simulado')
+plt.plot(B, magnetizacion, 'ok', label=r'Simulado')
 plt.plot(B, magnetizacion, '-r', label=r'Esperado')
 
 plt.xlabel(r'$B^{*}$', fontsize=AxisLabelSize)
@@ -52,29 +55,35 @@ plt.grid(axis='both', color='k', linestyle='dashed', linewidth=2, alpha=0.2)
 plt.show()
 plt.savefig('Magnetizacion_por_sitio.png')
 
-#%%
-# FALTA AGREGAR CUANDO ESTÉN HECHOS:
-
-# Datos de la simulación:
-# magnetizacion = np.loadtxt('   .csv', skiprows=1, delimiter=' ', unpack=True)
-# energia = np.loadtxt('   .csv', skiprows=1, delimiter=' ', unpack=True)
-
-# Valor esperado:
-B = np.linspace(0, 5, 100)
-energia = -B*np.tanh(B)*L*L
 
 #%%
 # Gráfico de energía:
 
 plt.figure()
-#plt.plot(, , 'ok', label=r'Simulado')
+plt.plot(B, energia, 'ok', label=r'Simulado')
 plt.plot(B, energia, '-r', label=r'Esperado')
 
 plt.xlabel(r'$B^{*}$', fontsize=AxisLabelSize)
-plt.ylabel(r'Magnetizaci\'on por sitio', fontsize=AxisLabelSize)
+plt.ylabel(r'Energ\'ia', fontsize=AxisLabelSize)
 plt.title(r'', fontsize=TitleSize)
 
 plt.legend(loc='best', fontsize=LegendSize)
 plt.grid(axis='both', color='k', linestyle='dashed', linewidth=2, alpha=0.2)
 plt.show()
 plt.savefig('Energia.png')
+
+#%%
+# Gráfico de susceptibilidad:
+
+plt.figure()
+#plt.plot(, , 'ok', label=r'Simulado')
+#plt.plot(B, susceptibilidad, '-r', label=r'Esperado')
+
+plt.xlabel(r'$B^{*}$', fontsize=AxisLabelSize)
+plt.ylabel(r'Susceptibilidad', fontsize=AxisLabelSize)
+plt.title(r'', fontsize=TitleSize)
+
+plt.legend(loc='best', fontsize=LegendSize)
+plt.grid(axis='both', color='k', linestyle='dashed', linewidth=2, alpha=0.2)
+plt.show()
+#plt.savefig('Susceptibilidad.png')
