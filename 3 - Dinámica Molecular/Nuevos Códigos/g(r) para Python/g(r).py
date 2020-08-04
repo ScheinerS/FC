15299cf85e87ade5fd9019738d4eb72990288483
SantamariaR <rami.santamaria92@gmail.com>
2020-08-04 12:53:57 -0300

    Datos g(r)

--- 3 - Dinámica Molecular/Nuevos Códigos/g(r) para Python/g(r).py
+++ 3 - Dinámica Molecular/Nuevos Códigos/g(r) para Python/g(r).py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 23 10:08:52 2020
+Created on Tue Aug  4 11:39:31 2020
 
 @author: ramiro
 """
@@ -23,7 +23,7 @@ TitleSize = 15
 AxisLabelSize = 15
 LegendSize = 12
 
-T_y_rho = [[0.4, 1.4], [0.8, 0.9], [0.8, 1.4], [1, 0.9], [1.5, 0.1]]
+T_y_rho = [[0.3, 0.36], [0.3, 0.46], [0.3, 0.58], [0.3, 0.75], [0.3, 0.90], [0.3, 1],[1.5, 0.36], [1.5, 0.46], [1.5, 0.58], [1.5, 0.75], [1.5, 0.90], [1.5, 1]]
 
 
 # Para una transición suave de colores entre las curvas:
@@ -35,16 +35,17 @@ rcParams['axes.prop_cycle'] = cycler(color=cmap(np.linspace(0, 1, N_curvas)))
 plt.figure()
 
 for t_r in T_y_rho:
-    T = t_r[0]
-    r = t_r[1]
-    data = np.loadtxt('DCM(T=%g,rho=%g).dat'%(T,r), skiprows=1, delimiter=' ', unpack=False)
+    T      = t_r[0]
+    r      = t_r[1]
+    
+    data = np.loadtxt('g(r)(N=1000,t=%g,rho=%g).dat'%(T,r), skiprows=0, delimiter=' ', unpack=False)
 
     #plt.plot(Temp, Pres, label=r'$\rho = %g$'%r)
 
-    iteracion = data[:, 0]
-    DCM = data[:, 1]
+    r    = data[:, 0]
+    g_r  = data[:, 1]
 
-    plt.plot(iteracion, DCM, label=r'$T = %g;\;\rho = %g$'%(T,r))
+    plt.plot(r, g_r, label=r'$T = %g;\;\rho = %g$'%(T,r))
 
 #plt.xlabel(r'Iteración', fontsize=AxisLabelSize)
 #plt.ylabel(r'Desplazamiento Cuadrático', fontsize=AxisLabelSize)
@@ -54,4 +55,4 @@ plt.legend(loc='best', fontsize=LegendSize)
 plt.grid(axis='both', color='k', linestyle='dashed', linewidth=2, alpha=0.2)
 plt.show()
 
-#plt.savefig('Gráficos/DCM.png')
+#plt.savefig('Gráficos/g(r).png')
